65294bf811284ede3d36e9d5e7de707459a697c9
prabal <prabal5ghosh@gmail.com>
2023-01-24 02:07:50 +0530

    blog app final commit

---
+++ blog/blog/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  # resources :articles
+  resources :articles do
+    resources :comments
+  end
+
+end
